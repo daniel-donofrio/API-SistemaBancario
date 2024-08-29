2647a7d0fd94fdae13bc84b2625f0fe4a1a4ac7c
Daniel D'Onofrio <danieldonofrio@live.com>
2024-08-29 20:22:48 -0300

    agrego CuentaDto

--- src/main/java/ar/edu/utn/frbb/tup/controladores/CuentaDto.java
+++ src/main/java/ar/edu/utn/frbb/tup/controladores/CuentaDto.java
@@ -1,5 +1,61 @@
 package ar.edu.utn.frbb.tup.controladores;
 
-public class CuentaDto {
+import ar.edu.utn.frbb.tup.modelo.TipoCuenta;
+import ar.edu.utn.frbb.tup.modelo.TipoMoneda;
 
+public class CuentaDto {
+    private long dniTitular;
+    private long cbu;
+    private TipoCuenta tipoCuenta;
+    private TipoMoneda tipoMoneda;
+    private String alias;
+    private double saldo;
+
+    public long getDniTitular() {
+        return dniTitular;
+    }
+
+    public void setDniTitular(long dniTitular) {
+        this.dniTitular = dniTitular;
+    }
+
+    public long getCbu() {
+        return cbu;
+    }
+
+    public void setCbu(long cbu) {
+        this.cbu = cbu;
+    }
+
+    public TipoCuenta getTipoCuenta() {
+        return tipoCuenta;
+    }
+
+    public void setTipoCuenta(TipoCuenta tipoCuenta) {
+        this.tipoCuenta = tipoCuenta;
+    }
+
+    public TipoMoneda getTipoMoneda() {
+        return tipoMoneda;
+    }
+
+    public void setTipoMoneda(TipoMoneda tipoMoneda) {
+        this.tipoMoneda = tipoMoneda;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 }
